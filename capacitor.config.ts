6c674544c6d8895d84452fef6f3c9cc05d2843f4
TongQuangThanh <tongquangthanh94@gmail.com>
2022-08-10 20:39:50 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'phim-online',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
